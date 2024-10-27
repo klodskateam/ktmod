4058350fef31b81b525dd0ec153ff65e029cb9bd
martinrokidomino <martinwin10b1909@gmail.com>
2024-10-27 23:28:06 +0200

    Обработчик руды

--- src/main/java/com/klodskateam/ktmod/init/KtmodModBlocks.java
+++ src/main/java/com/klodskateam/ktmod/init/KtmodModBlocks.java
@@ -14,6 +14,7 @@ import net.minecraftforge.api.distmarker.Dist;
 
 import net.minecraft.world.level.block.Block;
 
+import com.klodskateam.ktmod.block.UranOreBlock;
 import com.klodskateam.ktmod.block.TankBlock;
 import com.klodskateam.ktmod.block.SolderingStationBlock;
 import com.klodskateam.ktmod.block.ProcessedBedrockBlock;
@@ -34,6 +35,7 @@ public class KtmodModBlocks {
 	public static final RegistryObject<Block> TANK = REGISTRY.register("tank", () -> new TankBlock());
 	public static final RegistryObject<Block> PROCESSED_BEDROCK = REGISTRY.register("processed_bedrock", () -> new ProcessedBedrockBlock());
 	public static final RegistryObject<Block> SOLDERING_STATION = REGISTRY.register("soldering_station", () -> new SolderingStationBlock());
+	public static final RegistryObject<Block> URAN_ORE = REGISTRY.register("uran_ore", () -> new UranOreBlock());
 
 	@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 	public static class ClientSideHandler {
